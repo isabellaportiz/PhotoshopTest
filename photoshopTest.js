8eebc884dfc4a53fb4179b552abcdef22f9ba7c2
isabellaportiz <isabella_p_ortiz@hotmail.com>
2019-09-28 03:52:19 -0500

    Update photoshopTest.js

--- photoshopTest.js
+++ photoshopTest.js
@@ -1,3 +1,6 @@
-$(document).ready(function() {
-    alert("I made it!");
-})
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global $, window, location, CSInterface, SystemPath, themeManager*/
+
+function heythere() {
+    alert("Wassssup");
+}
